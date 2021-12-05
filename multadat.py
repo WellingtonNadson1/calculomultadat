fdf121dd16f485ef84775b3028ab42306419309e
Wellington Nadson <76890847+WellingtonNadson1@users.noreply.github.com>
2021-12-04 23:45:50 -0300

    Adequação no cálculo tornando o mesmo mais simplificado.
    Deixei de usar dicionário para usar apenas listas.

--- multadat.py
+++ multadat.py
@@ -1,32 +1,69 @@
+# Valor Base Bombeiro Militar
 vbbm = 27.61
 
+# Fator de Risco de Incêndio
 fator_risco =  [1, 1.1, 1.2]
 
-fator_area = [
-    {'200' : 4,
-    '> 200 ≤ 500' : 8,
-    '> 500 ≤ 750' :12,
-    '> 750 ≤ 1.500' : 16,
-    '> 1.500 ≤ 2.500' : 24,
-    '> 2.500 ≤ 3.500' : 30,
-    '> 3.500 ≤ 5.000' : 37,
-    '> 5.000 ≤ 7.000' : 43,
-    '> 7.000 ≤ 10.000' : 50,
-    '> 10.000 ≤ 20.000' : 56,
-    '> 20.000 ≤ 30.000' : 63,
-    '> 30.000 ≤ 40.000' : 69,
-    '> 40.000 ≤ 50.000' : 76,
-    '> 50.000 ≤ 60.000' : 83,
-    '> 60.000 ≤ 80.000' : 89,
-    '> 80.000 ≤ 100.000' : 94,
-    '> 100.000' : 100,
-    }
-]
+# Fator de Área Edificada
+fator_area = [4, 8, 12, 16, 24, 30, 37, 43, 50, 56, 63, 69, 76, 83, 89, 94, 100 ]
 
-infra_leve = [1]
-infra_media = [2]
-infra_grave = [3]
-infra_gravissima = [4,4]
+# Definindo as listas de infrações primeiramente vazias
+infra_leve = []
+infra_media = []
+infra_grave = []
+infra_gravissima = []
 
-multa = (2.5 * len(infra_leve) + 3.5 * len(infra_media) + 5 * len(infra_grave) + 7 * len(infra_gravissima)) * fator_risco[2] * vbbm
-print(multa)
+# Seleção do Risco Contra Incêndio da Edificação a ser Multada
+print('''
+Escolha qual o fator de Risco da Edificação Multada:
+
+0 - Risco Baixo (até 300 MJ/m²)
+1 - Risco Médio (Entre 300 e 1.200 MJ/m²)
+2 - Risco Alto (Acima de 1.200 MJ/m²)\n''')
+
+# Pegando o valor digitado pelo usuário e utilizando como índice para 
+# escolha do valor de risco da lista de fator de risco
+risco = fator_risco[int(input('Qual o Fator de Risco da Edificação? '))]
+
+
+# Apresentar a Seleção da Área da Edificação Vistoriada
+print('''
+Escolha o Tamanho da Área Edificada da Edificação a ser Multada:
+0 - (até 200)
+1 - (> 200 ≤ 500)
+2 - (> 500 ≤ 750)
+3 - (> 750 ≤ 1.500)
+4 - (> 1.500 ≤ 2.500)
+5 - (> 2.500 ≤ 3.500)
+6 - (> 3.500 ≤ 5.000)
+7 - (> 5.000 ≤ 7.000)
+8 - (> 7.000 ≤ 10.000)
+9 - (> 10.000 ≤ 20.000)
+10 - (> 20.000 ≤ 30.000)
+11 - (> 30.000 ≤ 40.000)
+12 - (> 40.000 ≤ 50.000)
+13 - (> 50.000 ≤ 60.000)
+14 - (> 60.000 ≤ 80.000)
+15 - (> 80.000 ≤ 100.000)
+16 - (> 100.000' : 100)\n''')
+
+area = fator_area[int(input('Qual o Tamanho da Área Edificada? '))]
+
+
+# Captando a quantidade de cada infração cometida e fazendo append na lista acima que foram criadas vazias.
+infra_leve2 = infra_leve.append(int(input('\nQuantas infrações leves foram cometidas? ')))
+
+infra_media2 = infra_media.append(int(input('\nQuantas infrações médias foram cometidas? ')))
+
+infra_grave2 = infra_grave.append(int(input('\nQuantas infrações graves foram cometidas? ')))
+
+infra_gravissima2 = infra_gravissima.append(int(input('\nQuantas infrações gravíssimas foram cometidas? ')))
+
+if infra_leve[0] <= 4 and infra_media[0] <= 4 and infra_grave[0] <= 4 and infra_gravissima[0] <= 2:
+    multa = (2.5 * infra_leve[0] + 3.5 * infra_media[0] + 5 * infra_grave[0] + 7 * infra_gravissima[0]) * risco * area * vbbm
+    print(f'\nA edificação fica Multada segundo a Lei 11.390/2020 no valor de R$ {multa}\n')
+else:
+    print('''
+Você deve atentar para a quantidade máxima permitida à cada nível 
+de infação a ser incluida no cálculo da Multa segundo a Lei 11.390.
+''')
